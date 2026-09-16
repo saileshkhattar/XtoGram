505f51f460b30ebcafd2d373d164d8b3763ee793
SaileshKhattar21 <sailesh@drapcode.com>
2026-09-16 16:18:47 +0530

    removed dead code

--- temp/routes/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { register, login } from '../controllers/authController'
-
-const authRouter = Router()
-
-authRouter.post('/register', register)
-authRouter.post('/login', login)
-
-export default authRouter
--- temp/routes/tweetRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { getTweet } from '../controllers/tweetController'
-
-const tweetRouter = Router()
-
-tweetRouter.post('/fetch', getTweet)
-
-export default tweetRouter;
